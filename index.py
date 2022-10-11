d02c926ab6b9aa7603b83c446afb386d5150323b
jagadeshroshan14 <115525438+jagadeshroshan14@users.noreply.github.com>
2022-10-11 12:04:10 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,12 @@
+# import the pandas library
+import pandas as pd
+left = pd.DataFrame({
+         'id':[1,2,3,4,5],
+         'Name': ['Alex', 'Amy', 'Allen', 'Alice', 'Ayoung'],
+         'subject_id':['sub1','sub2','sub4','sub6','sub5']})
+right = pd.DataFrame(
+         {'id':[1,2,3,4,5],
+         'Name': ['Billy', 'Brian', 'Bran', 'Bryce', 'Betty'],
+         'subject_id':['sub2','sub4','sub3','sub6','sub5']})
+print left
+print right
